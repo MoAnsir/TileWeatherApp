ae6a80884622542a69e28aa2729f8bf38606828e
Mo247 <mohammed.ansir@gmail.com>
2020-04-10 21:53:38 +0100

    Weather info component.

---
+++ tile-weather-app/src/components/WeatherInfo/WeatherInfo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './WeatherInfo.css';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faCloud } from "@fortawesome/free-solid-svg-icons";
+import { faCloudSun } from "@fortawesome/free-solid-svg-icons";
+import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
+import { faCloudShowersHeavy } from "@fortawesome/free-solid-svg-icons";
+import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
+
+const WeatherInfo = () => {
+  return (
+    <div className="weather-container">
+      <FontAwesomeIcon className="weather-icon" icon={faSnowflake} />
+      <h1>29oc</h1>
+      <h3>Sunny</h3>
+    </div>
+  );
+}
+
+export default WeatherInfo
--- tile-weather-app/src/containers/WeatherTile/WeatherTile.js
+++ tile-weather-app/src/containers/WeatherTile/WeatherTile.js
@@ -3,6 +3,7 @@ import './WeatherTile.css';
 
 import DayDate from '../../components/DayDate/DayDate';
 import Location from '../../components/Location/Location';
+import WeatherInfo from '../../components/WeatherInfo/WeatherInfo';
 
 class WeatherTile extends Component {
   render() {
@@ -11,6 +12,7 @@ class WeatherTile extends Component {
         <div className="weather-gradient">
           <DayDate />
           <Location />
+          <WeatherInfo />
         </div>
       </div>
     );
