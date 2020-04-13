64d4cb1e302348e7b221a0607e9816587d0ac282
Mo247 <mohammed.ansir@gmail.com>
2020-04-13 22:38:33 +0100

    fixed alighment

--- tile-weather-app/src/components/ForecastContainer/ForecastElement/ForecastElement.js
+++ tile-weather-app/src/components/ForecastContainer/ForecastElement/ForecastElement.js
@@ -10,23 +10,49 @@ Think of a better way to deliver the icon.
 */
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
-import { faCloud } from "@fortawesome/free-solid-svg-icons";
-import { faCloudSun } from "@fortawesome/free-solid-svg-icons";
-import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
-import { faCloudShowersHeavy } from "@fortawesome/free-solid-svg-icons";
-import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
+
 
 const ForecastElement = (props) => {
+  const forecastDetails = [
+    {
+      icon: "faSun",
+      day: "Tue",
+      temp: "29",
+      active: "active",
+    },
+    {
+      icon: "faCloud",
+      day: "Wed",
+      temp: "21",
+      active: "",
+    },
+    ,
+    {
+      icon: "faCloudShowersHeavy",
+      day: "Thur",
+      temp: "15",
+      active: "",
+    },
+    {
+      icon: "faCloudSun",
+      day: "Fri",
+      temp: "19",
+      active: "",
+    },
+  ];
+
   return (
     <div className="week-container">
       <ul className="week-list">
-        <li className={props.active}>
-          <FontAwesomeIcon className="weather-icon" icon={faSun} />
-          <span className="day-name">{props.day}</span>
-          <span className="day-temp">{props.temp}</span>
-        </li>
+        {forecastDetails.map((obj, index) =>{
+          return (
+            <li key={index} className={obj.active}>
+              <FontAwesomeIcon className="weather-icon" icon={obj.icon} />
+              <span className="day-name">{obj.day}</span>
+              <span className="day-temp celsius">{obj.temp}</span>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
